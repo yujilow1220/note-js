f4f3c3bded977c2bbff51e9d53c3d28cd22731a7
Takuya Takahashi <info@yujilow.net>
2016-04-23 22:45:09 +0900

    add help and runtime configuration

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 #!/usr/bin/env node
 
-
-// TODO: configの場所をどうするか
-// TODO: configのファイルをコマンドで編集できるようにする
 'use strict';
 var argv = require('argv');
-var config = require('config');
+var rc = require('runtime-configuration');
 
+if(!state || state === "-h" || state === "help"){
+	help();
+	return;
+}
 var targets = argv.run().targets;
 var state = targets.pop();
 
-switch(state){
+rc("note", function(err, config){
+  switch(state){
+	
+	  	case 'debug':
+	  		rc("note", function(err, config){
+				  			console.log(config);
+				  		});
+	  		break;
+	
+	  	case 'show':
+	  		console.log('this is show');
+	  		break;
+	
+	  	case 'config':
+	      var editor = require('child_process').spawn(config.editor, [process.env.HOME+'/.noterc'], {stdio: 'inherit'});
+	    	editor.on('exit', function() {
+				  			console.log("Configuration changed");
+				  		});
+	  		break;
+	
+	  	default:
+	  		var fs = require('fs');
+	  		var file = state+'.md';
+	      var editor = require('child_process').spawn(config.editor, [file], {stdio: 'inherit'});
+	  		editor.on('exit', function() {
+				});
+	  		break;
+	  }
+});
 
-	case 'debug':
-		console.log(process.env);
-		break;
 
-	case 'show':
-		console.log('this is show');
-		break;
+function help(){
+	console.log("\tUsage:");
+	console.log("\t\tnote [name]\t\t: create new note.");
+	console.log("\t\tnote show\t\t: show your list of notes.");
+	console.log("\t\tnote show [name]\t: show your note.");
+	console.log("\t\tnote config\t\t: edit '.noterc' config file.");
+	console.log("\t\tnote conifg example\t: show config example.");
 
-	case 'config':
-    var editor = require('child_process').spawn(config.editor, ['config/default.json'], {stdio: 'inherit'});
-		editor.on('exit', function() {
-		  var text = fs.readFileSync(file, 'utf-8');
-		  console.log(text);
-		});
-		break;
-
-	default:
-		var fs = require('fs');
-		var file = state+'.md';
-    var editor = require('child_process').spawn(config.editor, [file], {stdio: 'inherit'});
-		editor.on('exit', function() {
-		  var text = fs.readFileSync(file, 'utf-8');
-		  console.log(text);
-		});
-		break;
 }
-
-
