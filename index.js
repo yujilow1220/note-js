6328fdc6fb958302bb5ef6f4e11ebe9313078075
Takuya Takahashi <info@yujilow.net>
2016-04-23 23:29:35 +0900

    developed function of making note

--- index.js
+++ index.js
@@ -3,15 +3,21 @@
 'use strict';
 var argv = require('argv');
 var rc = require('runtime-configuration');
+var mkdirp = require('mkdirp');
+var fs = require('fs');
 
+var targets = argv.run().targets;
+var state = targets.shift();
 if(!state || state === "-h" || state === "help"){
 	help();
 	return;
 }
-var targets = argv.run().targets;
-var state = targets.pop();
 
 rc("note", function(err, config){
+
+	if(!config.save.dir_path){
+		config.save.dir_path = process.env.HOME+"/.note";
+	}
   switch(state){
 	
 	  	case 'debug':
@@ -21,7 +27,19 @@ rc("note", function(err, config){
 	  		break;
 	
 	  	case 'show':
-	  		console.log('this is show');
+				var title = targets.pop();
+				if(!title){
+					fs.readdir(config.save.dir_path, function(err, files){
+					    if (err) throw err;
+							files.forEach(function(file){
+								console.log(file);
+							});
+					});
+				}else{
+					//TODO: titleの中身をつなげて表示
+					return;
+				}
+
 	  		break;
 	
 	  	case 'config':
@@ -32,10 +50,13 @@ rc("note", function(err, config){
 	  		break;
 	
 	  	default:
-	  		var fs = require('fs');
-	  		var file = state+'.md';
-	      var editor = require('child_process').spawn(config.editor, [file], {stdio: 'inherit'});
-	  		editor.on('exit', function() {
+				var dir = config.save.dir_path+"/"+state;
+				mkdirp(dir, function(err){
+					if(err)return err;
+	  		  var file = dir+"/"+mkdate()+'.md';
+	        var editor = require('child_process').spawn(config.editor, [file], {stdio: 'inherit'});
+	  		  editor.on('exit', function() {
+				  });
 				});
 	  		break;
 	  }
@@ -50,4 +71,20 @@ function help(){
 	console.log("\t\tnote config\t\t: edit '.noterc' config file.");
 	console.log("\t\tnote conifg example\t: show config example.");
 
+}
+function mkdate(){
+	var date = new Date();
+	var year = date.getFullYear();  
+	var month = date.getMonth() + 1;  
+	var day = date.getDate();  
+	  
+	if ( month < 10 ) {  
+	　　month = '0' + month;  
+	}  
+	if ( day < 10 ) {  
+	　　day = '0' + day;  
+	}  
+	  
+	var str = year + '-' + month + '-' + day;
+	return str;
 }
