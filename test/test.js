792caed4ab8fcc034144c245fb69a6a327ee1f65
Takuya Takahashi <info@yujilow.net>
2016-05-02 16:21:52 +0900

    テストを編集

--- test/test.js
+++ test/test.js
@@ -1,3 +1,27 @@
-var note = require('../index.js');
+var note = require('../lib/note.js');
+var assert = require("chai").assert;
+var rewire = require('rewire');
+var note = rewire('../lib/note.js');
 
+describe('base', function(){
+	it('can get config', function(){
+		var config = note.config;
+		console.log(config.editor);
+		assert.equal(config.editor, 'vim');
+	});
 
+	it('can get path', function(){
+		
+	});
+});
+
+describe("show", function(){
+	it('should show some directory', function(){
+		
+	});
+});
+
+describe('edit', function(){
+	it('should show such directory', function(){
+	});	
+});
