5ae02c8cae2ecf0e224f4520934aaf0acca9ac24
Takuya Takahashi <info@yujilow.net>
2016-05-04 19:45:34 +0900

    すべてのメソッドをモジュールに移行

--- index.js
+++ index.js
@@ -20,39 +20,29 @@
  }
 
  var note = require('./lib/note.js');
- console.log(note.config);
+ note.init(function(){
+ 	switch(state){
 
- switch(state){
+ 		case 'debug':
+ 		break;
 
- 	case 'debug':
- 	break;
+ 		case 'show':
+ 		var title = targets.shift();
+ 		note.show(title);
+ 		break;
 
- 	case 'show':
- 	var title = targets.shift();
- 	note.show(title);
- 	break;
-
- 	case 'config':
- 	var editor = require('child_process').spawn(config.editor, [process.env.HOME+'/.noterc'], {stdio: 'inherit'});
- 	editor.on('exit', function() {
- 		console.log("Configuration changed");
- 	});
- 	break;
+ 		case 'config':
+ 		note.mkconfig();
+ 		break;
 
 
 			// 書いて保存するやつ
 			default:
-			var dir = config.save.dir_path+"/"+state;
-			mkdirp(dir, function(err){
-				if(err)return err;
-				var file = dir+"/"+mkdate()+'.md';
-				var editor = require('child_process').spawn(config.editor, [file], {stdio: 'inherit'});
-				editor.on('exit', function() {
-					push2Git(config.save.dir_path, file);
-				});
-			});
+			note.edit(state);
 			break;
 		}
+
+	});
 /**
  * ヘルプを出力
  *
@@ -66,31 +56,4 @@
  	console.log("\t\tnote config\t\t: edit '.noterc' config file.");
  	console.log("\t\tnote conifg example\t: show config example.");
 
- }
-
-/**
- * 今日の日付をフォーマットして出力
- *
- */
-
- function push2git(basedir, file){
- 	Git.Repository.open(basedir);
-
- }
-
- function mkdate(){
- 	var date = new Date();
- 	var year = date.getFullYear();
- 	var month = date.getMonth() + 1;
- 	var day = date.getDate();
-
- 	if ( month < 10 ) {
- 		　　month = '0' + month;
- 	}
- 	if ( day < 10 ) {
- 		　　day = '0' + day;
- 	}
-
- 	var str = year + '-' + month + '-' + day;
- 	return str;
  }
